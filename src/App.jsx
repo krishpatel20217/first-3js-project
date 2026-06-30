5dd745e9ad1a2e52286e43beeb43acda31edefbd
krishpatel20217 <krishpatel20217@gmail.com>
2026-06-30 19:55:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Dog from './components/Dog'
+import { Canvas } from '@react-three/fiber'
+
+
+function App() {
+  
+
+  return (
+    <Canvas>
+      <Dog/>
+    </Canvas>
+      
+    
+  )
+}
+
+export default App
